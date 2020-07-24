cd9937beb8209402ba11773638fdca62d278f979
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-24 22:39:00 +0300

    ft:getting_mean_scores:ml

--- text_class.py
+++ text_class.py
@@ -1,6 +1,6 @@
 import json
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 ###make a data class
 class Sentiment:
     NEGATIVE = "NEGATIVE"
@@ -44,29 +44,38 @@ test_x_vectors = vectorizer.transform(test_x)
 
 ##classification 
 ##LinearSVM
-# from sklearn import svm
+from sklearn import svm
 
-# clf_svm = svm.SVC(
-#     kernel='linear'
-# )
-# clf_svm.fit(train_x_vectors,train_y)
+clf_svm = svm.SVC(
+    kernel='linear'
+)
+clf_svm.fit(train_x_vectors,train_y)
 
-# clf_svm.predict(test_x_vectors)
+print(clf_svm.predict(test_x_vectors[0]))
+print(test_y[0])
 
 ###DECison Treee Classifier
-# from sklearn.tree import DecisionTreeClassifier
-# clf_dec = DecisionTreeClassifier()
-# clf_dec.fit(train_x_vectors,train_y)
-# clf_dec.predict(test_x_vectors)
+from sklearn.tree import DecisionTreeClassifier
+clf_dec = DecisionTreeClassifier()
+clf_dec.fit(train_x_vectors,train_y)
+print(clf_dec.predict(test_x_vectors[0]))
+print(test_y[0])
 
 ###Naive Bayes 
-# from sklearn.naive_bayes import GaussianNB 
-# clf_gnb = GaussianNB
-# clf_gnb.fit(train_x_vectors,train_y)
-# clf_gnb.predict(test_x_vectors)
+from sklearn.naive_bayes import GaussianNB 
+clf_gnb = GaussianNB()
+clf_gnb.fit(train_x_vectors.todense(),train_y)
+print(clf_gnb.predict(test_x_vectors.todense()[0]))
+print(test_y[0])
 
 ##Logistic Regression 
-# from sklearn.linear_model import LogisticRegression 
-# clf_log = LogisticRegression()
-# clf_log.fit(train_x_vectors,train_y)
-# clf_log.predict(test_x_vectors)
+from sklearn.linear_model import LogisticRegression 
+clf_log = LogisticRegression()
+clf_log.fit(train_x_vectors,train_y)
+print(clf_log.predict(test_x_vectors[0]))
+print(test_y[0])
+##Mean accureacy
+print(clf_svm.score(test_x_vectors,test_y))
+print(clf_dec.score(test_x_vectors,test_y))
+print(clf_gnb.score(test_x_vectors.todense(),test_y))
+print(clf_log.score(test_x_vectors,test_y))
