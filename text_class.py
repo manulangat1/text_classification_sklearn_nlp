ad76443db788b5c208d87f742726258f695aa8b0
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-24 19:24:30 +0300

    ft:adding_the_Review_class:ml

--- text_class.py
+++ text_class.py
@@ -1,12 +1,17 @@
 import json
+
+###make a data class
+class Review:
+    def __init__(self,text,score):
+        self.text=text
+        self.score=score
 reviews = []
 with open('./Books_small.json', 'r' ) as f:
     for line in f:
         review = json.loads(line)
-        reviews.append((review['reviewText'],review['overall']))
+        reviews.append(Review(review['reviewText'],review['overall']))
 
 print(len(reviews))
-print(reviews[5][1])
+print(reviews[5].text)
 
 
-###make a data class
